d4850d93a4a47a9c5a6214221466496dd8d1469c
Dharmendra0707 <Dharmendrapatel0707@gmail.com>
2018-09-16 10:59:12 +0530

    added tst.sh file

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "making foss folder"
+mkdir foss
+echo "Going to foss folder"
+cd foss
+echo "creating test_files.txt"
+touch test_files.txt
+echo ""
+echo "dhowing output of ls:"
+ls
+echo ""
+echo "updating linux repos"
+
